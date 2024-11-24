f8ddd9449b6a1964a79ab5cdf19dd3769f510d4c
Flamey123 <rytmer.samosa0u@icloud.com>
2024-11-24 19:28:07 +0100

    Update script.js klart til jul

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const wishlist = [
         {
-            name: 'GoPro Hero 10',
-            info: 'Actionkamera til å feks filme cubing.',
-            link: 'https://www.prisjakt.no/product.php?p=5715775'
+            name: 'GoPro Vanntett Deksel',
+            info: 'Vanntett deksel til GoPro.',
+            link: 'https://www.foto.no/gopro/138854/gopro-protective-housing-hero-10-11-12-vanntett-inntil-60m?pjclid=feeb31ed-22b0-43d2-88e5-8baaba2b26ad_e4c03e8bf0df72e5'
         },
         {
-            name: 'YJ MGC 7x7 M',
-            info: 'Dette er en magnetisk 7x7 cube.',
-            link: 'https://mastercubestore.no/yj-moyu/1438-yj-mgc-7x7-magnetic-stickerless-6970774553313.html'
-        },
-        {
-            name: 'Gan Lube II (Light)',
-            info: 'Silikonbasert glidemiddel til Rubiks cuber. Lav viskositet.',
-            link: 'https://mastercubestore.no/gancube/1925-gan-lube-ii-accel-10-ml-light.html'
-        },
-        {
-            name: 'Gan Lube III (Heavy)',
-            info: 'Silikonbasert glidemiddel til Rubiks cuber. Høy viskositet.',
-            link: 'https://mastercubestore.no/gancube/1926-gan-lube-iii-mastery-10-ml-heavy.html'
+            name: 'GoPro Stativ',
+            info: 'Stativ til GoPro.',
+            link: 'https://www.elkjop.no/product/sport-hobby-og-fritid/kamera-og-foto/kameratilbehor/tripod-til-kamera/fleksibelt-stativ-4i1-gopro-hero/60281'
         },
         {
             name: 'Nerdforge Chreative Chaos Journal',
@@ -26,24 +16,18 @@ document.addEventListener('DOMContentLoaded', function() {
             link: 'https://nerdforge.store/p/7918888485055'
         },
         {
-            name: 'Labyrint Duell Spill',
-            info: 'Brettspill for 2 spillere som handler om å slide brikker til riktig plass.',
-            link: 'https://www.finn.no/bap/forsale/ad.html?finnkode=349490765&ci=6'
-        },
-        {
-            name: 'One Piece Vol.3',
-            info: 'Dette er den tredje boka i en mangaserie(manga er japansk tegneserie).',
-            link: 'https://www.outland.no/one-piece-3'
-        },
-        {
-            name: 'One Piece Vol.2 Omnibus Edition',
+            name: 'One Piece Vol.3 Omnibus Edition',
             info: 'Dette er en 3 i 1 bok med 3 bøker fra en manga(manga er japansk tegneserie).',
-            link: 'https://www.outland.no/one-piece-3-in-1-4-5-6'
+            link: 'https://www.outland.no/one-piece-3-in-1-7-8-9'
         },
         {
             name: 'HoloNova Fan',
             info: 'Dette er en vifte som kan vise bilder og videoer i fin kvalitet. Hvis du skal kjøpe dette må du trykke på der det står "Plug type", og velge EU plug.',
             link: 'https://holonova.store'
+        },
+        {
+            name: 'VBucks/Robux',
+            info: 'Dette er en valutta som kan bli brukt i Fortnite og ROBLOX. Man kan bare overføre penger til Morten eller silje så blir det fikset senere.',
         },
         {
             name: 'Penger!',
