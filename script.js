67a8badbfa0643f6afe20ad11e206293793560ba
Flamey123 <rytmer.samosa0u@icloud.com>
2025-09-13 20:51:17 +0200

    Update script.js
    nye ønsker

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     const wishlist = [
         {
-            name: 'GoPro Stativ',
-            info: 'Stativ til GoPro.',
-            link: 'https://www.elkjop.no/product/sport-hobby-og-fritid/kamera-og-foto/kameratilbehor/tripod-til-kamera/fleksibelt-stativ-4i1-gopro-hero/60281'
+            name: 'YJ MGC Square-1',
+            info: 'Dette er en rar type Rubiks cube som endrer form med navnet "Square-1". Når du skal betale kan du bruke en rabatt kode som tilhører den beste Rubiks cube løseren i Norge. Koden er "KR".',
+            link: 'https://nordicube.no/products/yj-mgc-square-1-m?_pos=4&_psq=mgc&_ss=e&_v=1.0'
         },
         {
-            name: 'Nerdforge Chreative Chaos Journal',
-            info: 'Tegnebok med et spesielt design som kommer fra et YouTube par i Norge.',
-            link: 'https://nerdforge.store/p/7918888485055'
+            name: 'Moyu Weilong Skewb',
+            info: 'Dette er en type Rubiks cube som har en rar måte å vri på som heter en "Skewb".',
+            link: 'https://mastercubestore.no/yj-moyu/1957-moyu-weilong-skewb-maglev-stickerless-6970647060863.html'
         },
         {
-            name: 'One Piece Vol.3 Omnibus Edition',
-            info: 'Dette er en 3 i 1 bok med 3 bøker fra en manga(manga er japansk tegneserie).',
-            link: 'https://www.outland.no/one-piece-3-in-1-7-8-9'
+            name: 'QiYi Clock',
+            info: 'Dette er et Rubiks Pusslespill der målet er å få alle klokkene til å peke opp. Når du skal betale kan du bruke en rabatt kode som tilhører den beste Rubiks cube løseren i Norge. Koden er "KR". Hvis den ikke allerede er valgt, vil jeg helst ha Lilac fargen. Den kinesiske limited fargen er ikke lov i offisielle konkurannser.',
+            link: 'https://nordicube.no/products/qiyi-magnetic-clock?variant=46383443542233'
         },
         {
-            name: 'HoloNova Fan',
-            info: 'Dette er en vifte som kan vise bilder og videoer i fin kvalitet. Hvis du skal kjøpe dette må du trykke på der det står "Plug type", og velge EU plug.',
-            link: 'https://holonova.store'
+            name: 'VIN Cube 5x5',
+            info: 'Dette er en ganske billig 5x5 Rubiks cube med ganske premium følelse. Når du skal betale kan du bruke en rabatt kode som tilhører den beste Rubiks cube løseren i Norge. Koden er "KR".',
+            link: 'https://nordicube.no/products/vin-cube-5x5-m-uv'
         },
         {
             name: 'ASTRO Bot PS5',
             info: 'Dette er et spill til PlayStation 5.',
             link: 'https://www.elkjop.no/product/gaming/spill/nordisk-film-1000044860-game/788518'
-        },
-        {
-            name: 'VBucks/Robux',
-            info: 'Dette er en valutta som kan bli brukt i Fortnite og ROBLOX. Man kan bare overføre penger til Morten eller Silje så blir det fikset senere.',
         },
         {
             name: 'Penger!',
